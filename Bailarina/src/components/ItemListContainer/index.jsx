70c30b373bf74df0e4d1778350e73225d4329e77
JoaquinMontenegro2612 <JoaquinMontenegro2612@gmail.com>
2022-09-01 18:56:07 +0200

    item collection 2

--- Bailarina/src/components/ItemListContainer/index.jsx
+++ Bailarina/src/components/ItemListContainer/index.jsx
@@ -4,16 +4,17 @@ import ItemList from '../itemList/itemList'
 import {useParams} from 'react-router-dom'
 //comienzo uso firebase ↓↓
 import { db } from '../../Firebase/firebase'
-import { collection,getDocs } from 'firebase/firestore'
+import { collection, getDocs , query , where } from "firebase/firestore"
+import {toast} from "react-toastify"
 
 const ItemListContainer = () => {
     const [listaDeProductos, setListaDeProductos] = useState([])
     const [loading, setLoading] = useState(false)
+    const {id}=useParams()
   
-    const {categoria}=useParams()
 
     useEffect(() => {
-
+      if (!id){
       const coleccionDeProductos = collection(db, "Productos")
       const consulta = getDocs(coleccionDeProductos)
       
@@ -33,9 +34,33 @@ const ItemListContainer = () => {
             console.log(err);
             setLoading(false);
             })
-          },[])
+          } else {
+            const coleccionDeProductos = collection(db, "Productos")
+            const filtro = query(coleccionDeProductos,
+              where("categoria","==",id),
+              where("stock",">",10))
 
-          
+            const consulta = getDocs(filtro)
+
+            consulta
+            .then(snapshot=>{
+              const listaDeProductos = snapshot.docs.map(docs=>{
+                          return{
+                          ...docs.data(),
+                          id: docs.id
+                          }
+                      })
+                      setListaDeProductos(listaDeProductos);
+                      setLoading(false);
+                      })
+                      .catch(err=>
+                        {
+                        toast.error("Error al cargar los cocteles")
+                        })
+                      }
+},[id])
+
+  
 
 
 
